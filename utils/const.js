8b62a02213520e3a42afcc34649086d3a43c72a1
Shigatsi <shigatsii@gmail.com>
2021-03-17 23:19:08 +0300

    fix: allowedCors updated

--- utils/const.js
+++ utils/const.js
@@ -1,3 +1,8 @@
 module.exports.allowedCors = [
   'localhost:3000',
+  'http://shigatsimovie.students.nomoredomains.icu',
+  'http://www.shigatsimovie.students.nomoredomains.icu',
+  'https://shigatsimovie.students.nomoredomains.icu',
+  'https://www.shigatsimovie.students.nomoredomains.icu',
+
 ];
