eabf80304bba8ac028b4973e197e25e78029a839
Shigatsi <shigatsii@gmail.com>
2021-04-19 14:53:39 +0300

    fix: movie schema type of Id fixed

--- utils/const.js
+++ utils/const.js
@@ -1,5 +1,6 @@
 module.exports.allowedCors = [
   'localhost:3000',
+  'localhost:3001',
   'http://shigatsimovie.students.nomoredomains.icu',
   'http://www.shigatsimovie.students.nomoredomains.icu',
   'https://shigatsimovie.students.nomoredomains.icu',
