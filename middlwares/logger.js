ccd4a7b428382a1f2c69a2740a076a8dae92c34b
Shigatsi <shigatsii@gmail.com>
2021-04-22 18:46:18 +0300

    fix

--- middlwares/logger.js
+++ middlwares/logger.js
@@ -1,27 +1,27 @@
 const winston = require('winston');
-const format = require('winston');
+// const format = require('winston');
 const expressWinston = require('express-winston');
 
-const {
-  combine, timestamp, label, printf,
-} = format;
+// const {
+//   combine, timestamp, label, printf,
+// } = requformat;
 
-const myFormat = printf(({
-  level, message, label, timestamp,
-}) => `${timestamp} [${label}] ${level}: ${message}`);
+// const myFormat = printf(({
+//   level, message, label, timestamp,
+// }) => `${timestamp} [${label}] ${level}: ${message}`);
 
 // логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: 'request.log' })],
-  // format: winston.format.json(),
-  format: combine(label(), timestamp(), myFormat),
+  format: winston.format.json(),
+  // format: combine(label(), timestamp(), myFormat),
 });
 
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [new winston.transports.File({ filename: 'error.log' })],
-  // format: winston.format.json(),
-  format: combine(label({ label: 'does it work?' }), timestamp(), myFormat),
+  format: winston.format.json(),
+  // format: combine(label({ label: 'does it work?' }), timestamp(), myFormat),
 });
 
 module.exports = {
