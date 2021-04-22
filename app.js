0b6c256166f24d713d545229159c685767bcb941
Shigatsi <shigatsii@gmail.com>
2021-04-22 18:49:54 +0300

    change PORT to 3000

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
 
 const app = express();
-const { PORT = 3001, MONGO_URL, NODE_ENV } = process.env;
+const { PORT = 3000, MONGO_URL, NODE_ENV } = process.env;
 
-const routes = require('./routes/index');
-const limiter = require('./middlwares/limiter');
-const errorHandler = require('./middlwares/errorHandler');
-const { requestLogger, errorLogger } = require('./middlwares/logger');
-const { allowedCors, mongoDev } = require('./utils/const');
+const routes = require("./routes/index");
+const limiter = require("./middlwares/limiter");
+const errorHandler = require("./middlwares/errorHandler");
+const { requestLogger, errorLogger } = require("./middlwares/logger");
+const { allowedCors, mongoDev } = require("./utils/const");
 
 app.use(cors());
 app.use((req, res, next) => {
@@ -20,14 +20,14 @@ app.use((req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     // Проверяем, что значение origin есть среди разрешённых доменов
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     res.header(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE',
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE"
     );
   }
   next();
@@ -37,7 +37,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : mongoDev, {
+mongoose.connect(NODE_ENV === "production" ? MONGO_URL : mongoDev, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
