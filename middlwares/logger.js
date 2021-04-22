ceaa5df4c8222227fb61ae3fbe7108e6f34cbfcf
Shigatsi <shigatsii@gmail.com>
2021-04-22 18:11:58 +0300

    feat: logger format added

--- middlwares/logger.js
+++ middlwares/logger.js
@@ -1,20 +1,24 @@
 const winston = require("winston");
+const format = require("winston");
 const expressWinston = require("express-winston");
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
 
 // логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [new winston.transports.File({ filename: "request.log" })],
-  format: winston.format.json(),
+  // format: winston.format.json(),
+  format: combine(label(), timestamp(), myFormat),
 });
 
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [new winston.transports.File({ filename: "error.log" })],
   // format: winston.format.json(),
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.json()
-  ),
+  format: combine(label({ label: "does it work?" }), timestamp(), myFormat),
 });
 
 module.exports = {
